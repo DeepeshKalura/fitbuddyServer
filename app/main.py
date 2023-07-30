db5fd856e1b9aef8d6302bda4103f5cfddabef82
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-07-31 00:44:51 +0530

    update main

--- app/main.py
+++ app/main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from .database import engine
 from app import model
 from .router import post, user, auth
+from .config import settings
 
 
 # from fastapi.params import Body
 model.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+print(settings.DATABASE_NAME)
+print("/n/n/n/n/n/n/n/n/n/n/n/n")
+print(settings.DATABASE_USERNAME)
+print(settings.DATABASE_PASSWORD)
 
 app.include_router(post.router)
 app.include_router(user.router)
